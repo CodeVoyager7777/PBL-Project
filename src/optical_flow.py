0df31129531bed1f38ed04e17cd8ea84361c0952
Laksh Makkar <187654513+CodeVoyager7777@users.noreply.github.com>
2026-04-13 08:18:58 +0530

    Update optical_flow.py

--- src/optical_flow.py
+++ src/optical_flow.py
@@ -1,57 +1,19 @@
-import os
-import shutil
-import pandas as pd
+import cv2
+import numpy as np
 
-def sort_videos(video_folder, output_folder):
-    df = pd.read_excel("Results/final_results.xlsx")
+def compute_optical_flow(frames):
+    magnitudes = []
+    directions = []
 
-    threshold = df["stability_score"].mean()
-    print(f"Using threshold: {threshold:.2f}")
+    for i in range(1, len(frames)):
+        flow = cv2.calcOpticalFlowFarneback(
+            frames[i-1], frames[i],
+            None,
+            0.5, 3, 15, 3, 5, 1.2, 0
+        )
 
-    df["label"] = df["stability_score"].apply(
-        lambda x: "stable" if x > threshold else "unstable"
-    )
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        magnitudes.append(np.mean(mag))
+        directions.append(np.mean(ang))
 
-    os.makedirs("data", exist_ok=True)
-    df[["video_name", "label"]].to_excel("data/labels.xlsx", index=False)
-    print("labels.xlsx generated successfully!")
-
-    stable_path = os.path.join(output_folder, "stable")
-    unstable_path = os.path.join(output_folder, "unstable")
-
-    os.makedirs(stable_path, exist_ok=True)
-    os.makedirs(unstable_path, exist_ok=True)
-
-    available_files = {f.lower(): f for f in os.listdir(video_folder)}
-
-    for _, row in df.iterrows():
-        video_name = str(row["video_name"]).strip()
-
-        base_name = os.path.splitext(video_name)[0]
-        video_name = base_name + ".avi"
-
-        key = video_name.lower()
-
-        if key not in available_files:
-            continue
-
-        actual_name = available_files[key]
-        src_path = os.path.join(video_folder, actual_name)
-
-        label = row["label"]
-
-        if label == "stable":
-            dest_path = os.path.join(stable_path, actual_name)
-        else:
-            dest_path = os.path.join(unstable_path, actual_name)
-
-        shutil.move(src_path, dest_path)
-
-    print("Dataset sorting completed!")
-
-
-if __name__ == "__main__":
-    sort_videos(
-        video_folder="data/raw_videos",
-        output_folder="dataset"
-    )
+    return magnitudes, directions
