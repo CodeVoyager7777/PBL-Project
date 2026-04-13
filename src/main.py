73b7d14ddf6ddd6e23c6130cd43533f1b740da3c
Laksh Makkar <187654513+CodeVoyager7777@users.noreply.github.com>
2026-04-13 08:09:49 +0530

    Refactor folder paths and update results saving
    Refactored folder paths for data and results, removed report saving functionality, and added functionality to save results to an Excel file.

--- src/main.py
+++ src/main.py
@@ -4,34 +4,14 @@ from optical_flow import compute_optical_flow
 from feature_extraction import extract_features
 from stability_index import compute_stability_index
 from visualization import plot_motion
+import pandas as pd
 
-DATA_FOLDER = "D:/Probe Trajectory Project/Data/raw_videos"
-RESULTS_FOLDER = "D:/Probe Trajectory Project/Results"
-REPORT_FOLDER = "D:/Probe Trajectory Project/Results/motion reports"
-PLOT_FOLDER = "D:/Probe Trajectory Project/Results/plots"
+DATA_FOLDER = "data/raw_videos"
+RESULTS_FOLDER = "Results"
+REPORT_FOLDER = "Results/reports"
+PLOT_FOLDER = "Results/plots"
 
-def save_report(video_name, features, score):
-    os.makedirs(REPORT_FOLDER, exist_ok=True)
-
-    report_path = os.path.join(REPORT_FOLDER, f"{video_name}_report.txt")
-
-    with open(report_path, "w") as f:
-        f.write(f"Video: {video_name}\n\n")
-        f.write("Extracted Motion Features:\n")
-        for key, value in features.items():
-            f.write(f"{key}: {value}\n")
-
-        f.write(f"\nProbe Stability Index: {score} / 100\n")
-
-        if score > 80:
-            interpretation = "High stability."
-        elif score > 50:
-            interpretation = "Moderate stability with noticeable motion variation."
-        else:
-            interpretation = "Low stability with frequent abrupt movements."
-
-        f.write(f"\nInterpretation: {interpretation}\n")
-
+all_results = []
 
 def process_video(video_path):
     print(f"\nProcessing: {video_path}")
@@ -43,28 +23,29 @@ def process_video(video_path):
 
     video_name = os.path.basename(video_path).split(".")[0]
 
-    # Save plot
     plot_motion(magnitudes, video_name)
-
-    # Save report
-    save_report(video_name, features, stability_score)
 
     return features, stability_score
 
 
 def main():
     os.makedirs(PLOT_FOLDER, exist_ok=True)
-    os.makedirs(REPORT_FOLDER, exist_ok=True)
 
     for file in os.listdir(DATA_FOLDER):
         if file.endswith((".mp4", ".avi", ".mov")):
             video_path = os.path.join(DATA_FOLDER, file)
             features, score = process_video(video_path)
+            video_name = os.path.basename(video_path).split(".")[0]
+            result = {"video_name": video_name,"stability_score": score,**features}
+            all_results.append(result)
 
             print("Features:", features)
             print("Stability Index:", score, "/ 100")
 
     print("\nAll videos processed successfully.")
+    df = pd.DataFrame(all_results)
+    df.to_excel("Results/final_results.xlsx", index=False)
+    print("Results saved to Excel")
 
 
 if __name__ == "__main__":
